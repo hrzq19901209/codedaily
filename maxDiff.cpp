5aead76cc993d8d6f72639ef7f62dbd48780dfa8
hrzq19901209 <lol2012919@163.com>
2016-08-06 04:55:15 -0400

    动态规划求连续子序列的和的最大值

--- maxDiff.cpp
+++ maxDiff.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <limits.h>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -64,9 +66,63 @@ int maxDiff(int *start, int *end, int *max_, int *min_){
         return max(result, leftMax-rightMin);
     }
 }
+//假设最大的数值差由i, j指出
+//(a[i]-a[j]) = (a[i]-a[i+1]) + (a[i+1]-a[i+2]) +....+(a[j-1]-a[j])
+//所以将a数组转换成上面准话你后的,结果转换成求数组连续子数组的最大值问题
+int maxSequence(int array[], int size, int* start, int *end){
+    int result = array[0];
+    int current = array[0];
+    int cstart = 0,  cend = 0;
+    for(int i=1; i<size; i++){
+        if(current>0){
+            current += array[i];
+            cend = i;
+        }else{
+            current = array[i];
+            cstart = i;
+            cend = i;
+        }
+        if(current > result){
+            result = current;
+            *start = cstart;
+            *end = cend;
+        }
+    }
+    return result;
+}
+//通过动态数组直接求解，这样就不需要再分配空间了
+//实现方法， 改动上面的函数即可
+int maxDiff(int* array, int size, int* start, int* end){
+    int result = array[0]-array[1];
+    int current = result;
+    int cstart=0, cend=1;
+    int diff=0;
+    for(int i=1; i<size-1; i++){
+        diff = array[i]-array[i+1];
+        if(current > 0){
+            current += diff;
+            cend = i+1;
+        }else{
+            current = diff;
+            cstart = i;
+            cend = i;
+        }
+        if(current > result){
+            result = current;    
+            *start = cstart;
+            *end = cend;
+        }
+    }
+    return result;
+}
+
+void print(int n){
+    cout << n << " ";        
+}
 
 int main(){
     int a[] = {2, 4, 1, 16, 7, 5, 11, 9};
+    for_each(a, a+8, print);
     vector<int> array(a, a+8);
     int result = maxDiff(array, 0, array.size()-1);
     cout << result << endl;
@@ -75,5 +131,16 @@ int main(){
     result = maxDiff(a, a+7, &max_, &min_);
     cout << max_ << " " << min_ << endl;
     cout << result << endl;
+    int *b = new int[7]();
+    for(int i=0; i<7; i++){
+        b[i] = a[i]-a[i+1];   
+    }
+    int start, end;
+    cout << "最大差:" << maxSequence(b, 7, &start, &end) << endl;
+    cout << "start:" << start << "   end:" << end+1 << endl;
+    delete[] b;
+
+    cout << "最大差:" << maxDiff(a, 8 , &start, &end) << endl;
+    cout << "start:" << start << "   end:" << end << endl;
     return 0;
 }
