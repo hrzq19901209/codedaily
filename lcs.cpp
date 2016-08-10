8470895864fc64a65123643f3fa1d5d4390f11de
hrzq19901209 <lol2012919@163.com>
2016-08-10 04:28:05 -0400

    求子数组的最大和

---
+++ lcs.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int lcs(int a[], int n, int* start, int *end){
+    if(n < 1){
+        exit(1);    
+    }
+    int max = a[0];
+    int current = a[0];
+    int cstart = 0;
+    int cend = 0;
+    *start = cstart;
+    *end = cend;
+    for(int i=1; i<n; i++){
+        if(current > 0){
+            current += a[i];    
+            cend++;
+        }else{
+            current = a[i];
+            cstart = i;
+            cend = i;
+        }
+        if(current > max){
+            max = current;
+            *start = cstart;
+            *end = cend;
+        }
+    }
+    return max;
+}
+
+void print(int i){
+    cout << i << " ";
+}
+int main(int argc, char* argv[]){
+    int a[] = {1, -2, 3, 10, -4, 7, 2, -5};
+    for_each(a, a+8, print);
+    cout << endl;
+    int start, end;
+    cout << lcs(a, 8, &start, &end) << endl;
+    cout << "start:" << start << " end:"<< end << endl;
+    for_each(a+start, a+end+1, print);
+    cout << endl;
+    return 0;
+}
